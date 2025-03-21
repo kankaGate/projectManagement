dc537b20d0dda214232b63b46ddac3a50bbe765d
Luthfi <kankagate@gmail.com>
2025-03-21 08:52:22 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+function App() {
+  return (
+    <main>
+      <ProjectsSidebar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ add project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
